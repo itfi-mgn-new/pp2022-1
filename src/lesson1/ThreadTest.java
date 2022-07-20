ef93d919f841a20e12a2ccd9ed99ddd2832c2c69
chav1961 <chav1961@mail.ru>
2022-07-20 09:26:49 +0500

    Daily commit

--- src/lesson1/ThreadTest.java
+++ src/lesson1/ThreadTest.java
@@ -2,9 +2,24 @@ package lesson1;
 
 public class ThreadTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+		Thread	t = new Thread(()->{
+							while (!Thread.interrupted()) {
+								try {
+									Thread.sleep(2000);
+									System.err.println("T "+Thread.currentThread().getName());
+								} catch (InterruptedException exc) {
+									
+								}
+							}
+						});
+		t.setName("ASSA");
+		t.setDaemon(true);
+		t.start();
+		Thread.sleep(10000);
+		t.interrupt();
+		t.join();
 	}
 
 }
